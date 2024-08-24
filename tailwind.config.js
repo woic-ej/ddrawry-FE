3d15d30458802b6b84b664fc15d44ce21f9fcfb3
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-08-24 11:40:22 +0900

    Chore: 폰트 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        cookieRun: ["cookieRun"],
+      },
       colors: {
-        
-      }
+        Charcoal: "#36454F",
+        PrimaryStroke: "#94AFCA",
+        Primary: "#A7C7E7",
+        gray: "#767676",
+        LimeStroke: "#B4D28F",
+        Lime: "#E4FDC5",
+        black: "#000000",
+        LemonStroke: "#E0D35F",
+        Hightlight: "#E7C7E7",
+        Lemon: "#FFFACD",
+        ButtonDisabled: "#E6E6E6",
+        ButtonDisabledStroke: "#E1E1E1",
+      },
     },
   },
   plugins: [],
 };
-
