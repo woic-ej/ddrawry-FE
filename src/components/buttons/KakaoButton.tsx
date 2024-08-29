2f2c9cdd8f2573e91d4ac2d852dad613cdb31e38
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-08-29 21:21:52 +0900

    style: KakaoButton 컴포넌트 작업 완료

--- src/components/buttons/KakaoButton.tsx
+++ src/components/buttons/KakaoButton.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import kakaoIcon from "@assets/svgs/KakaoIcon.svg";
 
-const KakaoButton:React.FC = () => {
-  return (
-    <div>KakaoButton</div>
-  )
+interface KakaoButtonProps {
+  onClick: () => void;
 }
 
-export default KakaoButton
+const KakaoButton: React.FC<KakaoButtonProps> = ({ onClick }) => {
+  return (
+    <button
+      className="flex justify-center items-center gap-[8px] bg-[#FAE100] rounded-[8px] w-[435px] h-[60px] font-[500] text-[18px] leading-[25.2px]"
+      onClick={onClick}
+    >
+      <img src={kakaoIcon} alt="kakaoIcon" width={27} height={26} />
+      카카오로 로그인하기
+    </button>
+  );
+};
+
+export default KakaoButton;
