19e626221f51bdb172697893875fc1dd6b22c089
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-08-29 22:00:48 +0900

    refactor: text-[18px]를 text-small로 수정

--- src/components/buttons/KakaoButton.tsx
+++ src/components/buttons/KakaoButton.tsx
@@ -8,7 +8,7 @@ interface KakaoButtonProps {
 const KakaoButton: React.FC<KakaoButtonProps> = ({ onClick }) => {
   return (
     <button
-      className="flex justify-center items-center gap-[8px] bg-[#FAE100] rounded-[8px] w-[435px] h-[60px] font-[500] text-[18px] leading-[25.2px]"
+      className="flex justify-center items-center gap-[8px] bg-[#FAE100] rounded-[8px] w-[435px] h-[60px] font-[500] text-small leading-[25.2px]"
       onClick={onClick}
     >
       <img src={kakaoIcon} alt="kakaoIcon" width={27} height={26} />
