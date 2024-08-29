37c412089a03f6403a8081e50098a866f54693a7
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-08-29 21:52:22 +0900

    style: 버튼 공통 컴포넌트들 storybook 작업 완료

--- src/components/buttons/Button.stories.tsx
+++ src/components/buttons/Button.stories.tsx
@@ -1,5 +1,9 @@
 import { Meta, StoryObj } from "@storybook/react";
 import BigButton from "@components/buttons/BigButton";
+import SmallButton from "@components/buttons/SmallButton";
+import ModalButton from "./ModalButton";
+import KakaoButton from "./KakaoButton";
+import ToggleButton from "./ToggleButton";
 
 function Button() {
   return (
@@ -14,14 +18,35 @@ function Button() {
 
       <div className="flex flex-col gap-8">
         <h1 className="text-16 bg-white w-fit p-4">SmallButton</h1>
+        <SmallButton title="수정하기" color="green" />
+        <SmallButton title="일기 초기화" color="green" />
+        <SmallButton title="지우기" color="green" />
+        <SmallButton title="그림 지우기" color="green" />
+        <SmallButton title="그림 그려줘!" color="green" />
+        <SmallButton title="그림 그려줘!" color="gray" />
+      </div>
+
+      <div className="flex flex-col gap-8">
+        <h1 className="text-16 bg-white w-fit p-4">ModalButton</h1>
+        <ModalButton title="넹" />
+        <ModalButton title="이미지로" />
+        <ModalButton title="아니용" />
+        <ModalButton title="링크로" />
       </div>
 
       <div className="flex flex-col gap-8">
         <h1 className="text-16 bg-white w-fit p-4">ToggleButton</h1>
+        <ToggleButton leftTitle="캘린더형" rightTitle="목록형" />
+        <ToggleButton leftTitle="전체보기" rightTitle="날짜별" />
       </div>
 
       <div className="flex flex-col gap-8">
         <h1 className="text-16 bg-white w-fit p-4">KakaoButton</h1>
+        <KakaoButton
+          onClick={() => {
+            return;
+          }}
+        />
       </div>
     </div>
   );
