9ce96309edb98f4bc44d51fab3337477f43c06c9
woic-ej <woic0102@naver.com>
2024-09-02 21:36:49 +0900

    style: Calender 컴포넌트 스타일 오류 수정

--- src/pages/MainPage/components/calender/Calender.tsx
+++ src/pages/MainPage/components/calender/Calender.tsx
@@ -36,7 +36,7 @@ const renderCalenderItem = (day: Date, currentDate: Date, events: CalenderDataTy
 
 const Calender: React.FC<Props> = ({ currentMonthData, currentDate, calenderData }) => {
   return (
-    <div className="w-[1150px] h-[1067px] border-[3px] border-ButtonDisabled rounded-[10px] flex flex-col items-center gap-[54px]">
+    <div className="w-[1150px] min-h-[1067px] border-[3px] border-ButtonDisabled rounded-[10px] flex flex-col items-center gap-[54px]">
       <div className="flex w-full justify-around mt-[48px]">
         {DAY_LIST.map((day) => (
           <div
