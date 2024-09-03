06f17f18a140826709e89441d693c392d3da79d2
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-09-03 19:32:13 +0900

    style: 날씨 컴포넌트에 대한 storybook 작업 완료

---
+++ src/components/iconComponents/weather/WeatherList.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from "@storybook/react";
+import WeatherList from "@components/iconComponents/weather/WeatherList";
+import { useState } from "react";
+
+function WeatherListComponent() {
+  const [selectedWeather, setSelectedWeather] = useState<string | null>(null);
+  return (
+    <div className="flex gap-[30px]">
+      <span className="font-[400] leading-[48.96px] text-[36px] text-black">기분 : </span>
+      <WeatherList selectedWeather={selectedWeather} setSelectedWeather={setSelectedWeather} />
+    </div>
+  );
+}
+
+const meta: Meta<typeof WeatherListComponent> = {
+  title: "WeatherListComponent",
+  component: WeatherListComponent,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof WeatherListComponent>;
+
+export const Default: Story = {
+  args: {},
+};
