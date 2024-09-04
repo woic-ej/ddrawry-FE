d8ed75ebfe929b2c29fb0ec7763700533e71fa81
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-09-04 21:03:58 +0900

    style: font-[400]은 기본 폰트 weight이므로 스타일 지정 코드 삭제

--- src/pages/LoginPage/page/index.tsx
+++ src/pages/LoginPage/page/index.tsx
@@ -15,10 +15,10 @@ const LoginPage: React.FC = () => {
       <div className="flex-grow flex flex-col justify-center items-center gap-[80px]">
         <DefaultDiaryLogo />
         <div className="flex flex-col gap-[20px] ">
-          <div className="title-font font-[400] leading-[48.96px]">
+          <div className="title-font leading-[48.96px]">
             나만의 일상을 그림으로 그려주는 AI 그림 친구
           </div>
-          <div className="font-[400] text-[28px] leading-[38.08px] text-Primary">
+          <div className="text-[28px] leading-[38.08px] text-Primary">
             띠로리와 함께, 나의 하루를 그림으로 기록해요!
           </div>
         </div>
