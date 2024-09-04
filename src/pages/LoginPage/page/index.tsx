1d8a6002c34971ed128cd7c08191666acf317565
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-09-04 20:12:45 +0900

    style: 로그인 페이지 퍼블리싱 작업 완료

--- src/pages/LoginPage/page/index.tsx
+++ src/pages/LoginPage/page/index.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import KakaoButton from "@components/buttons/KakaoButton";
+import DefaultDiaryLogo from "@components/default/DefaultDiaryLogo";
+import DefaultHeader from "@components/header/DefaultHeader";
+import React from "react";
 
 const LoginPage: React.FC = () => {
-  return <div>LoginPage</div>;
+
+  const handleLogin = () => {
+    
+  }
+
+  return (
+    <>
+      <DefaultHeader title="띠로리" />
+      <div className="mt-[50px] flex flex-col justify-center items-center gap-[70px]">
+        <DefaultDiaryLogo />
+        <div className="flex flex-col gap-[20px] ">
+          <div className="title-font font-[400] leading-[48.96px]">
+            나만의 일상을 그림으로 그려주는 AI 그림 친구
+          </div>
+          <div className="font-[400] text-[28px] leading-[38.08px] text-Primary">
+            띠로리와 함께, 나의 하루를 그림으로 기록해요!
+          </div>
+        </div>
+        <KakaoButton onClick={handleLogin} />
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
