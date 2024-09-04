68b34c668c6b63634073b0e5ef46eb9243cdff1c
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-09-04 20:54:59 +0900

    style: 헤더 컴포넌트들 fixed top-0으로 고정 후 헤더 높이를 가지는 div를 선언해줌으로써 헤더의 공간이 채워짐

--- src/components/header/HeaderWithLike.tsx
+++ src/components/header/HeaderWithLike.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 
 const HeaderWithLike: React.FC = () => {
   return (
-    <div className="relative flex w-full p-0 h-[82px] justify-center items-center bg-Lemon">
-      <p className="text-center font-[400] title-font leading-[48.96px]">일기장</p>
-      <div className="absolute right-4">
-        <LikeIcon status={0} />
+    <>
+      <div className="fixed top-0 flex w-full p-0 h-[82px] justify-center items-center bg-Lemon">
+        <p className="text-center font-[400] title-font leading-[48.96px]">일기장</p>
+        <div className="absolute right-4">
+          <LikeIcon status={0} />
+        </div>
       </div>
-    </div>
+      {/* 헤더 높이만큼의 공간 확보 */}
+      <div className="h-[82px]" />
+    </>
   );
 };
 
--- src/components/header/HeaderWithProfile.tsx
+++ src/components/header/HeaderWithProfile.tsx
@@ -7,16 +7,20 @@ interface HeaderWithProfileProps {
 
 const HeaderWithProfile: React.FC<HeaderWithProfileProps> = ({ title }) => {
   return (
-    <div
-      className={`relative flex w-full p-0 h-[82px] justify-center items-center ${
-        title === "띠로리" ? "bg-Primary" : "bg-Lime"
-      }`}
-    >
-      <p className="text-center font-[400] title-font leading-[48.96px]">{title}</p>
-      <div className="absolute right-4">
-        <ProfileIcon />
+    <>
+      <div
+        className={`fixed top-0 flex w-full p-0 h-[82px] justify-center items-center ${
+          title === "띠로리" ? "bg-Primary" : "bg-Lime"
+        }`}
+      >
+        <p className="text-center font-[400] title-font leading-[48.96px]">{title}</p>
+        <div className="absolute right-4">
+          <ProfileIcon />
+        </div>
       </div>
-    </div>
+      {/* 헤더 높이만큼의 공간 확보 */}
+      <div className="h-[82px]" />
+    </>
   );
 };
 
