c5971971381837f6f0cdfd97fb4a671f63020ac0
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-09-05 17:12:55 +0900

    style: 메인 페이지 캘린더형 퍼블리싱 작업 완료

--- src/pages/MainPage/page/index.tsx
+++ src/pages/MainPage/page/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import ToggleButton from "@components/buttons/ToggleButton";
+import HeaderWithProfile from "@components/header/HeaderWithProfile";
+import SearchIcon from "@components/search/SearchIcon";
+import React from "react";
+import CalenderView from "../components/calender/CalenderView";
 
 const MainPage: React.FC = () => {
-  return <div>MainPage</div>;
+  return (
+    <div className="flex flex-col h-screen w-screen">
+      <HeaderWithProfile title="띠로리" />
+      <div className="flex flex-grow items-center flex-col gap-[48px] px-[130px] py-[53px]">
+        <div className="flex justify-between w-full min-w-[990px]">
+          <ToggleButton leftTitle="캘린더형" rightTitle="목록형" />
+          <button>
+            <SearchIcon />
+          </button>
+        </div>
+        <CalenderView />
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
