cc8c7d3ac45ad09e210697b021cb6d936f50efe0
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-09-09 20:16:50 +0900

    style: 메인 페이지의 html요소 너비가 실제 화면의 너비보다 작아서 스크롤이 생기던 오류 수정(메인페이지의 너비를 w-screen이 아닌 w-full로 수정)

--- src/pages/MainPage/page/index.tsx
+++ src/pages/MainPage/page/index.tsx
@@ -6,7 +6,7 @@ import CalenderView from "../components/calender/CalenderView";
 
 const MainPage: React.FC = () => {
   return (
-    <div className="flex flex-col h-screen w-screen">
+    <div className="flex flex-col h-screen w-full">
       <HeaderWithProfile title="띠로리" />
       <div className="flex flex-grow items-center flex-col gap-[48px] px-[130px] py-[53px]">
         <div className="flex justify-between w-full min-w-[990px]">
