15583374f778a42b69b4ba98dca6e1416b7f6bf0
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-09-09 20:07:22 +0900

    chore: toggleButton 컴포넌트에서 zustand로 title을 전역상태관리하며 그에 따른 toggleButton 컴포넌트 로직 수정

--- src/components/buttons/ToggleButton.tsx
+++ src/components/buttons/ToggleButton.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { useToggleStore } from "@store/useToggleStore";
+import React, { useEffect, useState } from "react";
 
 interface ToggleButtonProps {
   leftTitle: "캘린더형" | "전체보기";
@@ -7,21 +8,34 @@ interface ToggleButtonProps {
 
 const ToggleButton: React.FC<ToggleButtonProps> = ({ leftTitle, rightTitle }) => {
   const [activeButtonTitle, setActiveButtonTitle] = useState<string>(leftTitle);
+  const { setIsTotalView, setIsCalenderView, isTotalView, isCalenderView } = useToggleStore();
   const [isLeftActive, setIsLeftActive] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (leftTitle === "캘린더형") {
+      setIsLeftActive(isCalenderView);
+    } else {
+      setIsLeftActive(isTotalView);
+    }
+  }, [leftTitle]);
 
   const handleToggle = () => {
     if (activeButtonTitle === leftTitle) {
       if (leftTitle === "캘린더형") {
         setActiveButtonTitle("목록형");
+        setIsCalenderView(false);
       } else {
         setActiveButtonTitle("날짜별");
+        setIsTotalView(false);
       }
       setIsLeftActive(false);
     } else {
       if (rightTitle === "날짜별") {
         setActiveButtonTitle("전체보기");
+        setIsTotalView(true);
       } else {
         setActiveButtonTitle("캘린더형");
+        setIsCalenderView(true);
       }
       setIsLeftActive(true);
     }
@@ -31,7 +45,9 @@ const ToggleButton: React.FC<ToggleButtonProps> = ({ leftTitle, rightTitle }) =>
     <div className="flex rounded-[10px] border border-ButtonDisabledStroke w-[270px] h-[60px] text-regular leading-[38.08px]">
       <button
         onClick={() => {
-          !isLeftActive && handleToggle();
+          if (!isLeftActive) {
+            handleToggle();
+          }
         }}
         className={`w-[134px] h-[58px] rounded-[10px] ${
           isLeftActive ? "bg-PrimaryStroke text-white" : "bg-white text-Charcoal"
@@ -41,7 +57,9 @@ const ToggleButton: React.FC<ToggleButtonProps> = ({ leftTitle, rightTitle }) =>
       </button>
       <button
         onClick={() => {
-          isLeftActive && handleToggle();
+          if (isLeftActive) {
+            handleToggle();
+          }
         }}
         className={`w-[136px] h-[58px] rounded-[10px] ${
           isLeftActive ? "bg-white text-Charcoal" : "bg-PrimaryStroke text-white"
