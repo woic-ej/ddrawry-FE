63d674386d8ae54a684b17f13a20e523c96f678b
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-09-12 00:43:00 +0900

    fix: SearchInput 컴포넌트에서 받는 props 추가(SearchBar에서 받은 props 그대로 받음) 및 input에서 문자열을 입력하고 enter를 눌러도 내려받은 handleClick 함수가 호출되도록 추가

--- src/components/search/SearchInput.tsx
+++ src/components/search/SearchInput.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 
-const SearchInput: React.FC = () => {
+interface SearchInputProps {
+  content: string;
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleClick: () => void;
+}
+
+const SearchInput: React.FC<SearchInputProps> = ({ content, handleInputChange, handleClick }) => {
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleClick();
+    }
+  };
   return (
-    <div className="w-[1015px] h-[70px] rounded-[10px] border border-Gray flex items-center">
+    <div className="min-w-[1015px] max-w-[1067px] h-[70px] rounded-[10px] border border-Gray flex items-center">
       <input
         type="text"
-        className="w-[900px] placeholder-Gray text-regular ml-[24px] focus:outline-none"
+        className="min-w-[900px] max-w-[1013px] placeholder-Gray text-regular ml-[24px] focus:outline-none"
         placeholder="검색어를 입력하세요"
+        value={content}
+        onKeyDown={handleKeyDown}
+        onChange={(value) => handleInputChange(value)}
       />
     </div>
   );
