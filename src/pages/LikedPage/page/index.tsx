a2be1463012fd0d2f16ac1eac3da91a4d8fce344
woic-ej <woic0102@naver.com>
2024-10-06 23:41:34 +0900

    feat: 일기 데이터가 없을 경우 처리

--- src/pages/LikedPage/page/index.tsx
+++ src/pages/LikedPage/page/index.tsx
@@ -8,9 +8,10 @@ import DateManipulationBar from "@pages/MainPage/components/DateManipulationBar"
 import { useToggleStore } from "@store/useToggleStore";
 import React, { useEffect, useState } from "react";
 import { DiaryListType } from "src/types/diaryTypes";
+import EmptyState from "@components/empty/EmptyState";
 
 const LikedPage: React.FC = () => {
-  const [likedDiaries, setLikedDiaries] = useState<DiaryListType[]>();
+  const [likedDiaries, setLikedDiaries] = useState<DiaryListType[]>([]);
   const { currentDate, prevMonthHandler, nextMonthHandler } = useDateControl();
   const { isTotalView } = useToggleStore();
 
@@ -28,9 +29,13 @@ const LikedPage: React.FC = () => {
   }, []);
 
   return (
-    likedDiaries && (
-      <div className="flex flex-col h-screen w-full">
-        <HeaderWithProfile title="좋아요한 일기들" />
+    <div className="flex flex-col h-screen w-full">
+      <HeaderWithProfile title="좋아요한 일기들" />
+      {likedDiaries?.length === 0 ? (
+        <div className="w-full h-full">
+          <EmptyState message="좋아요한 일기가 없어요! 소중한 일기들을 하나씩 모아봐요" />
+        </div>
+      ) : (
         <div className="flex flex-grow items-center flex-col gap-[48px] px-[130px] py-[53px]">
           <div className="flex justify-start w-full min-w-[990px]">
             <ToggleButton leftTitle="전체보기" rightTitle="날짜별" />
@@ -43,11 +48,11 @@ const LikedPage: React.FC = () => {
                 nextMonthHandler={nextMonthHandler}
               />
             )}
-            <DiaryList diaries={likedDiaries} />
+            <DiaryList diaries={likedDiaries!} />
           </div>
         </div>
-      </div>
-    )
+      )}
+    </div>
   );
 };
 
