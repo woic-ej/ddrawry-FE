370f59d2273c321b729f34196aab8e1741a7d4b2
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-11-01 23:42:09 +0900

    chore: 회원 정보 조회하는 api 호출 함수 및 쿼리문 정의

---
+++ src/api/users/useConfirmProfile.ts
@@ -0,0 +1,26 @@
+import { apiRoutes } from "@api/apiRoutes";
+import api from "@api/fetcher";
+import { useQuery } from "@tanstack/react-query";
+
+type IGetResponseUserProfile = {
+  status: number;
+  message: string;
+  data: {
+    id: number;
+    nickname: string;
+    darkmode: boolean;
+    notification: boolean;
+  };
+};
+
+const fetchConfirmProfile = async () => {
+  const data = await api.get<IGetResponseUserProfile>({ endpoint: apiRoutes.userProfile });
+  return data;
+};
+
+export const useConfirmProfile = () => {
+    return useQuery({
+        queryKey: ['PROFILE'],
+        queryFn: () => fetchConfirmProfile(),
+    })
+}
