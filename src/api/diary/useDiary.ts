c53a2fae6c7496de24bae9a4b0af0878bc0167bb
woic-ej <woic0102@naver.com>
2024-11-01 23:41:50 +0900

    feat: 일기 수정 api 연동

--- src/api/diary/useDiary.ts
+++ src/api/diary/useDiary.ts
@@ -1,7 +1,8 @@
 import { apiRoutes } from "@api/apiRoutes";
 import api from "@api/fetcher";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
+import { DiaryFormData } from "src/types/WriteDiaryTypes";
 
 type DiaryResponse = {
   date: string;
@@ -14,10 +15,20 @@ type DiaryResponse = {
   image?: string;
 };
 
+type WriteDiaryPayLoad = {
+  date: string;
+  nickname: string;
+} & DiaryFormData;
+
+type WriteDiaryResponse = {
+  id: number;
+};
+
 export type EditDiaryResponse = {
   temp_id: number;
 };
 
+// 일기 조회
 const getDiary = async (diaryId: string) => {
   try {
     const { data }: { data: DiaryResponse } = await api.get({
@@ -30,6 +41,7 @@ const getDiary = async (diaryId: string) => {
   }
 };
 
+// 일기 삭제
 const deleteDiary = async (diaryId: string) => {
   try {
     await api.delete({ endpoint: `${apiRoutes.diary}/${diaryId}` });
@@ -39,6 +51,7 @@ const deleteDiary = async (diaryId: string) => {
   }
 };
 
+// 일기 수정 => 기존 임시데이터 사용 X 새로운 임시데이터 발급
 export const editDiary = async (diaryId: string) => {
   try {
     const { data }: { data: EditDiaryResponse } = await api.get({
@@ -51,10 +64,39 @@ export const editDiary = async (diaryId: string) => {
   }
 };
 
+// 일기 수정
+const updateDiary = async (diaryId: string, diaryData: WriteDiaryPayLoad) => {
+  try {
+    const { data }: { data: WriteDiaryResponse } = await api.put({
+      endpoint: `${apiRoutes.diary}/${diaryId}`,
+      body: diaryData,
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// 일기 작성
+const postDiary = async (diaryData: WriteDiaryPayLoad) => {
+  try {
+    const { data }: { data: WriteDiaryResponse } = await api.post({
+      endpoint: apiRoutes.diary,
+      body: diaryData,
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
 export const useGetDiary = (diaryId: string) => {
   return useQuery({
     queryKey: [`diary${diaryId}`],
     queryFn: () => getDiary(diaryId),
+    staleTime: 1000 * 60 * 5,
   });
 };
 
@@ -63,5 +105,34 @@ export const useDeleteDiary = (diaryId: string) => {
   return useMutation({
     mutationFn: () => deleteDiary(diaryId),
     onSuccess: () => navigate(-1),
+  });
+};
+
+export const useUpdateDiary = () => {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ diaryId, diaryData }: { diaryId: string; diaryData: WriteDiaryPayLoad }) =>
+      updateDiary(diaryId, diaryData),
+    onSuccess: (data) => {
+      navigate(-2);
+      queryClient.invalidateQueries({
+        queryKey: [`diary${data.id}`],
+      });
+    },
+  });
+};
+
+export const useWriteDiary = () => {
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: (diaryData: WriteDiaryPayLoad) => postDiary(diaryData),
+    onSuccess: (data) => {
+      navigate(`/diary/${data.id}`);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
   });
 };
