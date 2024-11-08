b22bca9132e3624be9f0b500d190f60c6d28033d
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-11-08 18:31:58 +0900

    style: 해당달의 달력에서 이전달, 다음달 미리보기 날짜에도 똑같이 데이터 표시되도록 로직 작성

--- src/pages/MainPage/components/calender/CalenderView.tsx
+++ src/pages/MainPage/components/calender/CalenderView.tsx
@@ -3,18 +3,21 @@ import DateManipulationBar from "@pages/MainPage/components/DateManipulationBar"
 import Calender from "@pages/MainPage/components/calender/Calender";
 import {
   addDays,
+  addMonths,
   differenceInCalendarDays,
   endOfMonth,
   endOfWeek,
   startOfMonth,
   startOfWeek,
+  subMonths,
 } from "date-fns";
 import { useDateControl } from "@hooks/useDateControl";
 import DiaryList from "@components/diary/list/DiaryList";
 import { useToggleStore } from "@store/useToggleStore";
 import { format } from "date-fns";
-import { useGetMainDiaries } from "@api/calender/useCalender";
 import EmptyState from "@components/empty/EmptyState";
+import { BaseDiaryType } from "src/types/diaryTypes";
+import { useGetMainDiaries } from "@api/calender/useCalender";
 
 const CalenderView: React.FC = () => {
   const { currentDate, prevMonthHandler, nextMonthHandler } = useDateControl();
@@ -23,14 +26,43 @@ const CalenderView: React.FC = () => {
   const startDate = startOfWeek(monthStart); // 현재 달의 시작 날짜가 포함된 주의 시작 날짜
   const endDate = endOfWeek(monthEnd); // 현재 달의 마지막 날짜가 포함된 주의 끝 날짜
   const { isCalenderView } = useToggleStore();
-  const [dateParam, setDateParam] = useState<string>(format(currentDate, "yyyyMM"));
-  const { data: getMainCalender } = useGetMainDiaries(isCalenderView, dateParam);
-  const currentMonth = currentDate.getMonth() + 1
-  console.log(getMainCalender)
-  
+  const [currentDateParam, setCurrentDateParam] = useState<string>(format(currentDate, "yyyyMM"));
+  const [previousMonthParam, setPreviousMonthParam] = useState<string>(
+    format(subMonths(currentDate, 1), "yyyyMM"),
+  );
+  const [nextMonthParam, setNextMonthParam] = useState<string>(
+    format(addMonths(currentDate, 1), "yyyyMM"),
+  );
+
+  // 해당달 캘린더 data
+  const { data: getCurrentMainCalender } = useGetMainDiaries(isCalenderView, currentDateParam);
+
+  // 다음달 캘린더 data
+  const { data: getNextMainCalender } = useGetMainDiaries(isCalenderView, nextMonthParam);
+
+  // 이전달 캘린더 data
+  const { data: getPreviousMainCalender } = useGetMainDiaries(isCalenderView, previousMonthParam);
+
+  const [getTotalCalenderData, setGetTotalCalenderData] = useState<BaseDiaryType[]>();
+
   useEffect(() => {
-    setDateParam(format(currentDate, "yyyyMM"));
-  }, [currentMonth])
+    if (getCurrentMainCalender && getNextMainCalender && getPreviousMainCalender) {
+      setGetTotalCalenderData([
+        ...getCurrentMainCalender.data,
+        ...getNextMainCalender.data,
+        ...getPreviousMainCalender.data,
+      ]);
+    }
+    
+  }, [getCurrentMainCalender, getNextMainCalender, getPreviousMainCalender, currentDateParam]);
+
+  const currentMonth = currentDate.getMonth() + 1;
+
+  useEffect(() => {
+    setCurrentDateParam(format(currentDate, "yyyyMM"));
+    setNextMonthParam(format(addMonths(currentDate, 1), "yyyyMM"));
+    setPreviousMonthParam(format(subMonths(currentDate, 1), "yyyyMM"));
+  }, [currentMonth]);
 
   const currentMonthData = useMemo(() => {
     const monthArray = [];
@@ -49,19 +81,19 @@ const CalenderView: React.FC = () => {
         prevMonthHandler={prevMonthHandler}
         nextMonthHandler={nextMonthHandler}
       />
-      {getMainCalender &&
+      {getTotalCalenderData && getCurrentMainCalender && 
         (isCalenderView === true ? (
           <Calender
             currentMonthData={currentMonthData}
             currentDate={currentDate}
-            calenderData={getMainCalender.data}
+            calenderData={getTotalCalenderData}
           />
-        ) : getMainCalender.data.length === 0 ? (
+        ) : getCurrentMainCalender.data.length === 0 ? (
           <div className={"h-full w-full flex flex-grow justify-center items-center"}>
             <EmptyState message="작성된 일기가 없어요!" />
           </div>
         ) : (
-          <DiaryList diaries={getMainCalender.data} />
+          <DiaryList diaries={getCurrentMainCalender.data} />
         ))}
     </div>
   );
