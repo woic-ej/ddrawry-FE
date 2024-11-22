3bc39a7a6af059866c5195a3c2e1016175440dbf
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-11-22 19:29:20 +0900

    fix: 일기 공유 모달에서 이미지 공유 삭제하고 멘트 수정

--- src/pages/DiaryPage/components/DiaryButtonSection.tsx
+++ src/pages/DiaryPage/components/DiaryButtonSection.tsx
@@ -83,11 +83,11 @@ const DiaryButtonSection = ({ date, diaryId }: Props) => {
       {isShareModalOpen && (
         <ModalLayout setIsModalOpen={setIsShareModalOpen}>
           <DefaultModal
-            title="짱 멋진 일기를 어떻게 자랑할까요?"
-            leftText="이미지로"
-            rightText="링크로"
+            title="짱 멋진 일기를 링크로 자랑할까요?"
+            leftText="넹"
+            rightText="아니용"
             leftClick={handleLinkSharedDiary}
-            rightClick={handleLinkSharedDiary}
+            rightClick={() => setIsShareModalOpen(false)}
           />
         </ModalLayout>
       )}
