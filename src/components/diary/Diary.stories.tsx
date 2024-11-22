d39f078247c0d56a221102a83fe40acd794708e7
woic-ej <woic0102@naver.com>
2024-11-22 15:32:47 +0900

    fix: 타입에러 수정

--- src/components/diary/Diary.stories.tsx
+++ src/components/diary/Diary.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from "@storybook/react";
 import Diary from "@components/diary/Diary";
 
 function DiaryComponent() {
-  return <Diary date="2024년 8월 10일" nickname="최은진" count={3} isFull={false} />;
+  return <Diary date="2024년 8월 10일" nickname="최은진" />;
 }
 
 const meta: Meta<typeof DiaryComponent> = {
