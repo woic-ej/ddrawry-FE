76c10672eba6e7b03c4a65a524b8a64e77010fea
woic-ej <woic0102@naver.com>
2024-12-06 19:42:12 +0900

    fix: 배포,개발 환경  로직 다시 수정

--- src/api/users/useLogin.ts
+++ src/api/users/useLogin.ts
@@ -12,7 +12,7 @@ const IS_DEV = window.location.hostname === "localhost";
 const fetchLogin = async (code: string) => {
   try {
     const { data }: { data: FetchLoginResponse } = await api.get({
-      endpoint: `${apiRoutes.login}?${IS_DEV && "dev=1&"}code=${code}`,
+      endpoint: `${apiRoutes.login}?${IS_DEV ? "dev=1&" : ""}code=${code}`,
     });
     return data;
   } catch (error) {
